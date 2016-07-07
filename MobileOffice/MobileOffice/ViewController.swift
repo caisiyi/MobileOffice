28fd1c0ee5d411b3c027ccc69f2fe6af69c65a4d
caisiyi <494040854@qq.com>
2016-07-07 10:57:29 +0800

    初始化版本

---
+++ MobileOffice/MobileOffice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileOffice
+//
+//  Created by 蔡斯仪 on 16/7/7.
+//  Copyright © 2016年 caisiyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
